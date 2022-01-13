51e75be8b1aa7f1419758ecceab00790dd39e99c
Savser91 <savser91@gmail.com>
2022-01-13 20:29:16 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import com.company.intsCalculator.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
+        System.out.println(intsCalc.sub(25, 5));
+        System.out.println(intsCalc.div(30, 3));
+    }
+}
